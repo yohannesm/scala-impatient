dd51c47124f6541ba8e954ceab2343c58d33930d
Marcell Himawan <mhimawan@hiya.com>
2019-07-18 00:09:59 -0700

    started ch 13

--- chapter13/CollectionsApp.scala
+++ chapter13/CollectionsApp.scala
@@ -1,5 +1,19 @@
-
+import scala.collection.immutable._
+import scala.collection.mutable.ListBuffer
+import scala.io.Source
 
 object CollectionsApp extends App {
 
+  def indexMapToSet(str: String) = {
+    import scala.collection.mutable._
+    val charsToIndices = new HashMap[Char, Set[Int]] with MultiMap[Char, Int] {
+      override protected def makeSet: Set[Int] = new TreeSet[Int]
+    }
+    for (i <- str.indices) charsToIndices.addBinding(s(i), i)
+    charsToIndices
+  }
+
+  val strMiss = "Mississippi"
+
+  println( indexMapToSet(strMiss))
 }
