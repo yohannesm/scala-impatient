6e0230c75bca387e66cc4449d5e1524c0aa6aacd
Marcell Himawan <mhimawan@hiya.com>
2019-08-26 15:25:31 -0700

    added more trees

--- chapter14/TreeApp.scala
+++ chapter14/TreeApp.scala
@@ -11,4 +11,17 @@ object TreeApp extends App {
   val lst5 = List(List(3, 8), 2, List(5))
 
   println( leafSum(lst5) )
+
+  object Task6 {
+    sealed trait BinaryTree
+
+    case class Leaf(value: Int) extends BinaryTree
+
+    case class Node(left: BinaryTree, right: BinaryTree) extends BinaryTree
+
+    def leafSum(bt: BinaryTree): Int = bt match {
+      case leaf: Leaf => leaf.value
+      case node: Node => leafSum(node.left) + leafSum(node.right)
+    }
+  }
 }
