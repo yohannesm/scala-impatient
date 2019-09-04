aa15997ec27cd42cbdfeda3a8a5492f3923ef92d
Marcell Himawan <mhimawan@hiya.com>
2019-09-04 14:17:27 -0700

    trying out lower type bound

---
+++ scratch-space/LowerTypeBoundsApp.scala
@@ -0,0 +1,20 @@
+
+
+object LowerTypeBoundsApp extends App {
+
+trait Node[+B] {
+  def prepend[U >: B](elem: U): Node[U]
+}
+
+case class ListNode[+B](h: B, t: Node[B]) extends Node[B] {
+  def prepend[U >: B](elem: U): ListNode[U] = ListNode(elem, this)
+  def head: B = h
+  def tail: Node[B] = t
+}
+
+case class Nil[+B]() extends Node[B] {
+  def prepend[U >: B](elem: U): ListNode[U] = ListNode(elem, this)
+}
+
+
+}
